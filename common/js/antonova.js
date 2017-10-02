184e204abf6c344b559191cf20ed87475735d04e
aleksey.movchanyuk <aleksey.movchanyuk@gmail.com>
2017-10-02 12:07:00 +0200

    Submit button added

--- common/js/antonova.js
+++ common/js/antonova.js
@@ -3,3 +3,7 @@ lightGallery(document.getElementById('lightgallery'), {
     animateThumb: false,
     showThumbByDefault: false
 });
+
+// bind to the submit event of our form
+var form = document.getElementById('gform');
+form.addEventListener("submit", handleFormSubmit, false);
